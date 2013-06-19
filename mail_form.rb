51224147bc73bb16785abad462945370b2a1cbfc
Chip Castle <chip@chipcastle.com>
2013-06-19 21:08:28 +0000

    Add comments, program2 and reformat

--- mail_form.rb
+++ mail_form.rb
@@ -16,18 +16,27 @@ class MailForm < Sinatra::Base
               from:    settings['no_reply'],
               subject: settings['subject'],
               body:    <<-EOT
-      Name:     #{params[:name]}
-      Address:  #{params[:address]}
-      City:     #{params[:city]}
-      State:    #{params[:state]}
-      Zip:      #{params[:zipcode]}
-      Phone:    #{params[:phone]}
-      Email:    #{params[:email]}
-      Child:    #{params[:child1]}     Date of Birth: #{params[:child1_dob]}
-      Child:    #{params[:child2]}     Date of Birth: #{params[:child2_dob]}
-      Program of Interest:                            #{params[:program]}
+      Parent Information:
+      Name: #{params[:name]}
+      Address: #{params[:address]}
+      City: #{params[:city]}
+      State: #{params[:state]}
+      Zip: #{params[:zipcode]}
+      Phone: #{params[:phone]}
+      Email: #{params[:email]}
+
+      Children:
+      Name: #{params[:child1]}
+      Date of Birth: #{params[:child1_dob]}
+      Program of Interest: #{params[:program]}
+
+      Name: #{params[:child2]}
+      Date of Birth: #{params[:child2_dob]}
+      Program of Interest: #{params[:program2]}
+
       What elementary school does your child attend?: #{params[:school]}
-      How did you hear about us?:                     #{params[:referral]}
+      How did you hear about us?: #{params[:referral]}
+      Additional comments and/or questions: #{params[:comments]}
     EOT
     
     redirect to(settings['site'])
